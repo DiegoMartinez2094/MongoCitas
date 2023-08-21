d1b22dda5d1b9ef80be6f4cfc4e0aa7ecd638b48
Diego <Diegofernandomartinezcalderon@gmail.com>
2023-08-21 17:42:33 -0500

    feat: :sparkles: encontrar un usuario por su id
    se implementa el metodo usuario para buscar un usuario especifico por su id

--- routers/usuario.js
+++ routers/usuario.js
@@ -1,14 +1,33 @@
 import { con } from "../db/atlas.js";
-import {Router} from "express";
+import { Router } from "express";
 
-const appUsuario =Router();
+const appUsuario = Router();
 let db = await con();
-let usuario = db.collection('usuario');
+let usuario = db.collection("usuario");
 
-appUsuario.get("/", async(req, res) => { 
-    let db = await con();
-    let usuario = db.collection('usuario');
-    let result = await usuario.find().sort({ usu_nombre: 1 }).toArray();
-    res.send(result); });
+appUsuario.get("/:usu_id?", async (req, res) => {
+    const usu_id = req.params.usu_id ? parseInt(req.params.usu_id) : null;
 
-export default appUsuario; 
+    try {
+        let db = await con();
+        let usuario = db.collection('usuario');
+        
+        if (usu_id !== null) {
+            let result = await usuario.findOne({ usu_id });
+            if (result) {
+                res.send(result);
+            } else {
+                res.status(404).send("Usuario no encontrado");
+            }
+        } else {
+            let result = await usuario.find().sort({ usu_nombre: 1 }).toArray();
+            res.send(result);
+        }
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).send("Error interno del servidor");
+    }
+});
+
+
+export default appUsuario;
