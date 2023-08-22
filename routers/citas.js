cd7eb6d03db9a5d741baf400a52e8476b6005dae
Diego <Diegofernandomartinezcalderon@gmail.com>
2023-08-21 19:30:04 -0500

    feat: :sparkles: implementacion de metodo obtener citas
    se obtienen citas por fechas

--- routers/citas.js
+++ routers/citas.js
@@ -1,14 +1,35 @@
 import { con } from "../db/atlas.js";
-import {Router} from "express";
+import { Router } from "express";
 
-const appCitas =Router();
+const appCitas = Router();
 let db = await con();
-let cita = db.collection('cita');
+let cita = db.collection("cita");
 
-appCitas.get("/", async(req, res) => { 
-    let db = await con();
-    let cita = db.collection('cita');
-    let result = await cita.find().sort({ cit_codigo: 1 }).toArray();
-    res.send(result); });
+appCitas.get("/", async (req, res) => {
+  let db = await con();
+  let cita = db.collection("cita");
+  let result = await cita.find().sort({ cit_codigo: 1 }).toArray();
+  res.send(result);
+});
 
-export default appCitas; 
+appCitas.get("/fecha/:cit_fecha", async (req, res) => {
+    const cit_fecha = req.params.cit_fecha;
+
+    try {
+        let db = await con();
+        let cita = db.collection('cita');
+        if (cit_fecha) {
+            let result = await cita.find({ "cit_fecha": new Date(cit_fecha) }).toArray();
+            res.send(result);
+        } else {
+            res.status(400).send("Fecha de cita inválida");
+        }
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).send("Error interno del servidor");
+    }
+});
+
+
+
+export default appCitas;
