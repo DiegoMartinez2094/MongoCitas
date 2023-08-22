f7c69482e28b6ad6f9b8abdf47adce35ef898015
Diego <Diegofernandomartinezcalderon@gmail.com>
2023-08-21 20:06:14 -0500

    feat: :sparkles: Modificacion del router medico
    se modifica para obtener con un endpoint especifico el nombre de los medicos junto a sus consultorios

--- routers/medico.js
+++ routers/medico.js
@@ -5,26 +5,34 @@ const appMedico =Router();
 let db = await con();
 let medico = db.collection('medico');
 
-appMedico.get("/:esp_nombre?", async (req, res) => {
+appMedico.get("/especialidad/:esp_nombre", async (req, res) => {
     const esp_nombre = req.params.esp_nombre;
 
     try {
         let db = await con();
         let medico = db.collection('medico');
         
-        if (esp_nombre) {
-            let result = await medico.find({ "med_especialidad.esp_nombre": esp_nombre }).toArray();
-            res.send(result);
-        } else {
-            let result = await medico.find().toArray();
-            res.send(result);
-        }
+        let result = await medico.find({ "med_especialidad.esp_nombre": esp_nombre }).toArray();
+        res.send(result);
     } catch (error) {
         console.error("Error:", error);
         res.status(500).send("Error interno del servidor");
     }
 });
 
+
+appMedico.get("/medicosYconsultorios", async (req, res) => {
+    try {
+        let db = await con();
+        let medico = db.collection("medico");
+
+        let result = await medico.find().project({ _id: 0, med_nombreCompleto: 1, "med_consultorio.cons_nombre": 1 }).toArray();
+        res.send(result);
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).send("Error interno del servidor");
+    }
+});
 
 
 export default appMedico; 
